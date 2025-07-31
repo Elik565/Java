86d9942720a58de4648582bec6f5eeb190213546
elik565 <eliseysavich@gmail.com>
2025-07-31 14:26:07 +0300

    Множественное наследование интерфейсов

--- src/pkg1/Main1.java
+++ src/pkg1/Main1.java
@@ -1,11 +1,29 @@
 package pkg1;
 
+// В интерфейсе все методы по умолчанию public
+interface A {
+    void SayHello();
+}
+
+interface B {
+    void SayGoodbye();
+}
+
+// Объявляем, что класс C должен реализовать все методы из интерфейсов A и B
+class C implements A, B {  
+    public void SayHello() {
+        System.out.println("Hello");
+    }
+
+    public void SayGoodbye() {
+        System.out.println("Goodbye");
+    }
+}
+
 public class Main1 {
     public static void main(String[] args) {
-        PublicClass pc = new PublicClass();
-        pc.print();
-
-        PackagePrivateClass ppc = new PackagePrivateClass();
-        ppc.print();
+        C obj = new C();
+        obj.SayHello();
+        obj.SayGoodbye();
     }
 }
